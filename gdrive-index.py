9f50f511d431dc75be928416709962e2b30587ff
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-04-16 13:55:20 +0530

    gdrive-index:

--- gdrive-index.py
+++ gdrive-index.py
@@ -29,6 +29,7 @@ def generateDirArray(dirToScan):
     dirIDsDictionary[dirToScan] = 0
     for currentDir, dirs, files in os.walk(dirToScan):
         for dir in dirs:
+            print(i+'/'+len(dirs))
             dirIDsDictionary[currentDir+'/'+dir] = i
             i = i + 1
 
