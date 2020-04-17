fe3c831bb1442ebb9dd5f5d2c01e2737e2156c32
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-04-17 20:01:05 +0530

    --- Auto-Git Commit ---

--- test.py
+++ test.py
@@ -1,7 +1,2 @@
-from time import sleep
-from IPython.display import clear_output, display
-
-for f in range(10000):
-    clear_output(wait=True)
-    print(f)  # use display(f) if you encounter performance issues
-    # sleep(1)
+a = 76515486515/1048576
+print(a)
